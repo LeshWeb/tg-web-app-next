a842419db336f44353061e89afe6959b73faa2df
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-16 21:25:35 +0400

    fix five

--- src/app/hooks/useTelegramMiniApps.ts
+++ src/app/hooks/useTelegramMiniApps.ts
@@ -1,13 +1,6 @@
 import { miniApp } from '@telegram-apps/sdk-react';
 import { useEffect } from 'react';
 
-declare global {
-  interface Window {
-    Telegram?: {
-      WebApp?: any;
-    };
-  }
-}
 
 export function useTelegramMiniApp() {
   useEffect(() => {
