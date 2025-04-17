49cf60310a6d567da140f96a31471a9c417d590e
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-17 21:28:59 +0400

    btn fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useTelegramMiniApp } from "./hooks/useTelegramMiniApps";
-import { Button } from "./components/Button";
 import { Header } from "./components/Header";
 
 export default function Home() {
