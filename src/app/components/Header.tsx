76c0c7d7a8b73659fd725b8ae098b728d404dc22
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-17 22:48:20 +0400

    initData

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,16 @@
-'use client";';
+"use client";
 import { postEvent } from "@telegram-apps/sdk-react";
 import { Button } from "./Button";
-import { retrieveLaunchParams } from "@telegram-apps/sdk";
+import { useIsTG } from "../hooks/useIsTG";
 
 export function Header() {
-  const tg = window.Telegram.WebApp;
+  const initData = useIsTG();
+  console.log("initData", initData);
 
   return (
     <div className="w-full h-12 flex items-center px-5">
       <Button onClick={() => postEvent("web_app_close", {})}>Закрыть</Button>
-      <span>{tg.initDataUnsafe?.user?.username}</span>
+      <span>{}</span>
     </div>
   );
 }
