60c6efa570745ffe2b7c3bbaaaf2350ee01dfaf9
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-17 23:10:14 +0400

    refix header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <div className="w-full h-12 flex items-center px-5">
       <span>
         {isTMA()
-          ? window.Telegram.WebApp.initDataUnsafe?.user?.username
+          ? window.Telegram.WebApp.initDataUnsafe?.user?.first_name
           : "Telegram Web App"}
       </span>
       <Button onClick={() => postEvent("web_app_close", {})}>Закрыть</Button>
