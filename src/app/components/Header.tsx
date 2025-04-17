63736a6dad44e7d0ce46b9776e64948108fda732
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-17 23:11:46 +0400

    tma header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,11 +5,7 @@ import { Button } from "./Button";
 export function Header() {
   return (
     <div className="w-full h-12 flex items-center px-5">
-      <span>
-        {isTMA()
-          ? window.Telegram.WebApp.initDataUnsafe?.user?.first_name
-          : "Telegram Web App"}
-      </span>
+      <span>{isTMA() ? "TMA" : "Telegram Web App"}</span>
       <Button onClick={() => postEvent("web_app_close", {})}>Закрыть</Button>
     </div>
   );
