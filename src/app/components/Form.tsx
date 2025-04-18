dc55e42e0d570fa9e70d9462548f983b22f3e49b
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-18 11:51:12 +0400

    no use

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useTelegram } from "../hooks/useTelegram";
 
 export function Form() {
