016581936a161bc7707a75424c36a4f12f9acbdf
Aleksey Cherkasov <279123165a@gmail.com>
2025-04-18 21:11:57 +0400

    fix callback

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useTelegram } from "../hooks/useTelegram";
 import { sendData } from "@telegram-apps/sdk";
 
